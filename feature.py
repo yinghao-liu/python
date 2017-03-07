573653c7617628f270bd990284957734fb5e8c87
yinghao-liu <yinghao_liu@163.com>
2017-03-07 15:15:34 -0800

    数据类型

--- feature.py
+++ feature.py
@@ -1,5 +1,15 @@
-#!/usr/bin/python2.6
-import urllib
-f=urllib.urlopen("http://m.cnblogs.com/")
-s=f.read()
-print s
+#!/usr/bin/python
+a = 1
+b = 1.23
+c = True
+d = 1+2j
+e = r'aa%\n'
+f = []
+g = (1,)
+h = {1,2}
+i = {}
+
+print(e)
+print(type(a),type(b),type(c),type(d),type(e))
+print(type(f),type(g),type(h),type(i))
+
