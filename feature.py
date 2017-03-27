a3aa46e3417f4f7c24b228da0cd426af8ba53a24
yinghao-liu <yinghao_liu@163.com>
2017-03-27 21:18:37 +0800

    web server include http and https

--- feature.py
+++ feature.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python
+# generate https.pem with following command:
+# openssl req -new -x509 -keyout https.pem -out https.pem -days 3650 -nodes
+# 
+
+
 import BaseHTTPServer
+import SimpleHTTPServer
+import thread
+import ssl
+import sys
+
+HTTPS_PORT = 443
+HTTP_PORT  = 8000
+
 
 class RequestHandle(BaseHTTPServer.BaseHTTPRequestHandler):
 	Page = """\
 		<html>
 			<body>
-				<p>hello,web!</p>
+				<p>this is https server</p>
 			</body>
 		</html>
 		"""
@@ -14,10 +27,30 @@ class RequestHandle(BaseHTTPServer.BaseHTTPRequestHandler):
 		self.send_header("Content-Type","text/html")
 		self.send_header("Content-Length",str(len(self.Page)))
 		self.end_headers()
-		#self.wfile.write(self.Page)
-		self.wfile = self.Page
+		self.wfile.write(self.Page)
 
-server=BaseHTTPServer.HTTPServer(('0.0.0.0',8000),RequestHandle)
-server.serve_forever()
+def https_server(PORT):
+	server=BaseHTTPServer.HTTPServer(('0.0.0.0',PORT),RequestHandle)
+	server.socket=ssl.wrap_socket(server.socket, certfile='./https.pem', server_side=True)
+	server.serve_forever()
 
 
+def http_server(PORT):
+	#server=BaseHTTPServer.HTTPServer(('0.0.0.0',PORT),SimpleHTTPServer.SimpleHTTPRequestHandler)
+	server=BaseHTTPServer.HTTPServer(('0.0.0.0',PORT),RequestHandle)
+	server.serve_forever()
+
+def main():
+	#thread.start_new_thread(https_server, (HTTPS_PORT,))
+	thread.start_new_thread(http_server, (HTTP_PORT,))
+	while True:
+		pass
+
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt:
+		print "you kill me!"
+		sys.exit()
+	except SSLError:
+		pass
