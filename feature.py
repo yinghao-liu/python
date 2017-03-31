ff0f1c13e0473946d2d6cdc5f58ab914de5c4ea9
yinghao-liu <yinghao_liu@163.com>
2017-03-31 22:48:51 +0800

    web server

--- feature.py
+++ feature.py
@@ -1,35 +1,23 @@
 #!/usr/bin/python
-from xml.dom.minidom import parse
-import xml.dom.minidom
+import BaseHTTPServer
 
-# open xml with minidom
-DOMTree = xml.dom.minidom.parse("movies.xml")
-collection = DOMTree.documentElement
-if collection.hasAttribute("shelf"):
-   print ("Root element : %s" % collection.getAttribute("shelf"))
+class RequestHandle(BaseHTTPServer.BaseHTTPRequestHandler):
+	Page = """\
+		<html>
+			<body>
+				<p>hello,web!</p>
+			</body>
+		</html>
+		"""
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header("Content-Type","text/html")
+		self.send_header("Content-Length",str(len(self.Page)))
+		self.end_headers()
+		#self.wfile.write(self.Page)
+		self.wfile = self.Page
 
-# get all the movies 
-movies = collection.getElementsByTagName("movie")
-
-# print details of every movies 
-for movie in movies:
-   print ("*****Movie*****")
-   if movie.hasAttribute("title"):
-      print ("Title: %s" % movie.getAttribute("title"))
-
-   type = movie.getElementsByTagName('type')[0]
-   print ("Type: %s" % type.childNodes[0].data)
-   format = movie.getElementsByTagName('format')[0]
-   print ("Format: %s" % format.childNodes[0].data)
-   rating = movie.getElementsByTagName('rating')[0]
-   print ("Rating: %s" % rating.childNodes[0].data)
-   description = movie.getElementsByTagName('description')[0]
-   print ("Description: %s" % description.childNodes[0].data)
+server=BaseHTTPServer.HTTPServer(('0.0.0.0',8000),RequestHandle)
+server.serve_forever()
 
 
-
-
-if __name__ == "__main__":
-	print("main")
-else:
-	print("module")
