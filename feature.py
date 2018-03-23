8d0134a3f1470dcef1c42d904363d2a2b6f989e3
yinghao-liu <yinghao_liu@163.com>
2018-03-23 17:50:23 +0800

    变长参数表

--- feature.py
+++ feature.py
@@ -1,20 +1,7 @@
 #!/usr/bin/env python3
-import xml.dom.minidom
-xml_file = "config.xml"
-def xml_config_init():
-	with xml.dom.minidom.parse(xml_file) as dom:
-		sub_list = dom.getElementsByTagName("sub")
-		for sub_index in range(sub_list.length):
-			sub = sub_list.item(sub_index)
-
-			item_list = sub.childNodes
-			print(item_list)
-			print(item_list.length)
-			for item_index in range(item_list.length):
-				item = item_list.item(item_index)	
-
-				print(item.nodeType, ":", item)
-
+def printf(*arg):
+	for val in arg:
+		print(val)
 
 if "__main__" == __name__:
-	xml_config_init()
+	printf(1,2,3,4)
