828b278a2a186f2b02a3800ac98fbfc69d2d4242
francis <yinghao_liu@163.com>
2018-08-10 10:38:24 +0000

    tf programmers_guide/low_level_intro

--- feature.py
+++ feature.py
@@ -1,50 +1,23 @@
 #!/usr/bin/env python3
+import tensorflow as tf
 
-class name:
-	"""doccccccccc"""
-	def __init__(self):
-		self.va="va"	# instance variable unique to each instance
-		self.va_mutable=["va"]
-	def print_name(self):
-		print("hello there")
+x = tf.constant([[1], [2], [3], [4]], dtype=tf.float32)
+y_true = tf.constant([[0], [-1], [-2], [-3]], dtype=tf.float32)
 
-	"""second doccc"""
-	__aa="pseudo private aa"
-	bb="public immutable bb" # class variable shared by all instances in initial, see the differences between mutable and immutable variables 
-	__aa_mutable=["aa_mutable"]
-	bb_mutable=["bb_mutable"]
+linear_model = tf.layers.Dense(units=1)
 
+y_pred = linear_model(x)
+loss = tf.losses.mean_squared_error(labels=y_true, predictions=y_pred)
 
-if "__main__" == __name__:
-	obj1=name();
-	obj1.print_name()
-	print(obj1.__doc__)
-	print(obj1.va)
-	print(obj1.va_mutable)
-	#this is called name mangling, it still is possible to access or modify a variable that is considered private.
-	print(obj1._name__aa)
-	print(obj1._name__aa_mutable)
-	print(obj1.bb)
-	print(obj1.bb_mutable)
+optimizer = tf.train.GradientDescentOptimizer(0.08)
+train = optimizer.minimize(loss)
 
-	print("----------------------")
-	obj2=name();
-	obj1.va="va after modify"
-	obj1.va_mutable.append("va_mutable after modify")
-	obj1._name__aa="private aa, after modify"
-	obj1._name__aa_mutable.append("aa_mutable after modify")
-	obj1.bb="class variable bb after modify"
-	obj1.bb_mutable.append("bb_mutable after modify")
-	print(obj1.va)
-	print(obj1.va_mutable)
-	print(obj1._name__aa)
-	print(obj1._name__aa_mutable)
-	print(obj1.bb)
-	print(obj1.bb_mutable)
-	print("+++++++++++++++++++++")
-	print(obj2.va)
-	print(obj2.va_mutable)
-	print(obj2._name__aa)
-	print(obj2._name__aa_mutable)
-	print(obj2.bb)
-	print(obj2.bb_mutable)
+init = tf.global_variables_initializer()
+
+sess = tf.Session()
+sess.run(init)
+for i in range(150):
+	_, loss_value = sess.run((train, loss))
+	print(loss_value)
+
+print(sess.run(y_pred))
