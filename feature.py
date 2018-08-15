66db38d51b9443999985b6c42188d46349b3bf6a
francis <yinghao_liu@163.com>
2018-08-15 07:13:22 +0000

    lib json (dumps and loads are enough, I thought)

--- feature.py
+++ feature.py
@@ -1,38 +1,20 @@
 #!/usr/bin/env python3
-from http.server import HTTPServer
-from http.server import BaseHTTPRequestHandler
-HTTP_PORT  = 8000
 
-class RequestHandle(BaseHTTPRequestHandler):
-	Page = """\
-		<html>
-		<body>
-		<p>this is http server</p>
-		</body>
-		</html>
-		"""
-	def do_GET(self):
-		print(type(self.wfile))
-		print(type(self.rfile))
-		self.send_response(200)
-		self.send_header("Content-Type","text/html")
-		self.send_header("Content-Length",str(len(self.Page)))
-		self.end_headers()
-		self.wfile.write(bytes(self.Page, encoding="utf8"))
-	
+import json
 
-	def do_POST(self):
-		leng=self.headers["Content-Length"]
-		print(self.rfile.read(int(leng)))
-		self.send_response(200)
-		self.send_header("Content-Type","text/html")
-		self.send_header("Content-Length",str(len(self.Page)))
-		self.end_headers()
-		self.wfile.write(bytes(self.Page, encoding="utf8"))
+python_obj={}
+python_obj['name']='francis'
+python_obj["age"]=18
 
-def http_server(PORT):
-	server=HTTPServer(('0.0.0.0', PORT), RequestHandle)
-	server.serve_forever()
+json_obj='{"name":"evan", "age":20}'
 
-if "__main__" == __name__:
-	http_server(HTTP_PORT)
+dumps=json.dumps(python_obj)
+loads=json.loads(json_obj)
+print(type(dumps))
+print(dumps)
+
+print(type(loads))
+print(loads)
+
+
+
