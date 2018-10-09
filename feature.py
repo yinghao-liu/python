cfaa048609841c1fa764f2b263ff0e0c0e8ed173
francis <yinghao_liu@163.com>
2018-10-09 09:05:20 +0000

    subprocess和timeout(shell=True的影响)

--- feature.py
+++ feature.py
@@ -2,22 +2,24 @@
 import subprocess
 
 if "__main__" == __name__:
-	subprocess.run("ps -ef |  grep init | grep -v grep".split(), shell=True)
-	ret=subprocess.run("ping -c2 baidu.com".split(), timeout=3, capture_output=True)
-	print(ret)
-	try:
-		subprocess.run("ping baidu.com".split(), timeout=3)
-	except subprocess.TimeoutExpired as a:
-		print("process should been killed".format(a))
-	try:
-		"""
-		when shell is True, timeout can not kill the program(ping, below), it can only kill its parent process(the shell)
-		refer to https://stackoverflow.com/questions/48763362/python-subprocess-kill-with-timeout
-		"""
-		subprocess.run("ping baidu.com", shell=True, timeout=3)
-	except subprocess.TimeoutExpired as b:
-		print("------{}-------".format(b))
+    subprocess.run("ps -ef |  grep init | grep -v grep".split(), shell=True)
+    ret=subprocess.run("ping -c2 baidu.com".split(), timeout=3, capture_output=True)
+    print(ret)
+    try:
+         subprocess.run("ping baidu.com".split(), timeout=3)
+    except subprocess.TimeoutExpired as a:
+        print("process should been killed".format(a))
+        try:
+            """
+            when shell is True, timeout can not kill the program(ping, below), it can only kill its parent process(the shell)
+            refer to https://stackoverflow.com/questions/48763362/python-subprocess-kill-with-timeout
+            """
+            subprocess.run("ping baidu.com", shell=True, timeout=3)
+        except subprocess.TimeoutExpired as b:
+            print("------{}-------".format(b))
+            print()
 
-	
+
+
 
 
