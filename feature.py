78206418df892710cf7061723532f81d488bdca1
francis <yinghao_liu@163.com>
2018-10-12 02:48:33 +0000

    scope_test

--- feature.py
+++ feature.py
@@ -1,25 +1,27 @@
-#!/usr/bin/env python3.7
-import subprocess
+#!/usr/bin/env python3
+
+value=100
+def scope_test():
+    value=10 # this is for 'nonlocal' in do_nonlocal
+    def do_local():
+        value=1
+        print("this is in do local, value is {}".format(value))
+
+    def do_nonlocal():
+        nonlocal value # The nonlocal statement causes the listed identifiers to refer to previously bound variables in the nearest enclosing scope excluding globals.
+        value+=1
+        print("this is in do_nonlocal value is {}".format(value))
+
+    def do_global():
+        global value
+        value+=1
+        print("this is in do_global, value is {}".format(value))
+    do_local()
+    do_nonlocal()
+    do_global()
 
 if "__main__" == __name__:
-    subprocess.run("ps -ef |  grep init | grep -v grep".split(), shell=True)
-    ret=subprocess.run("ping -c2 baidu.com".split(), timeout=3, capture_output=True)
-    print(ret)
-    try:
-         subprocess.run("ping baidu.com".split(), timeout=3)
-    except subprocess.TimeoutExpired as a:
-        print("process should been killed".format(a))
-        try:
-            """
-            when shell is True, timeout can not kill the program(ping, below), it can only kill its parent process(the shell)
-            refer to https://stackoverflow.com/questions/48763362/python-subprocess-kill-with-timeout
-            """
-            subprocess.run("ping baidu.com", shell=True, timeout=3)
-        except subprocess.TimeoutExpired as b:
-            print("------{}-------".format(b))
-            print()
-
-
-
+    scope_test()
+    print("this is in main, value is {}".format(value))
 
 
