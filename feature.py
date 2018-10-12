3eb260456135f6bdf677ffed65803368499973ba
francis <yinghao_liu@163.com>
2018-10-12 11:10:54 +0000

    instance object

--- feature.py
+++ feature.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python3
 
-value=100
-def scope_test():
-    value=10 # this is for 'nonlocal' in do_nonlocal
-    def do_local():
-        value=1
-        print("this is in do local, value is {}".format(value))
+class dog:
+    data="this is dog data"
+    def init(self, name):
+        self.kind=name
 
-    def do_nonlocal():
-        nonlocal value # The nonlocal statement causes the listed identifiers to refer to previously bound variables in the nearest enclosing scope excluding globals.
-        value+=1
-        print("this is in do_nonlocal value is {}".format(value))
-
-    def do_global():
-        global value
-        value+=1
-        print("this is in do_global, value is {}".format(value))
-    do_local()
-    do_nonlocal()
-    do_global()
 
 if "__main__" == __name__:
-    scope_test()
-    print("this is in main, value is {}".format(value))
+    a=dog()
+    b=dog()
+    print(dog.init)
+    print(a.init)
+    a.init("wang")
+    b.init("miao")
+    print(a.kind)
+    print(b.kind)
 
+    a.kk=123
+    b.kk=234
+    print(a.kk)
+    print(b.kk)
 
