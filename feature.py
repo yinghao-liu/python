0574948bc125146d4185f3e17ff74f47d977d8ed
francis <yinghao_liu@163.com>
2018-10-15 08:15:12 +0000

    pseudo private

--- feature.py
+++ feature.py
@@ -1,23 +1,11 @@
 #!/usr/bin/env python3
 
 class dog:
-    data="this is dog data"
-    def init(self, name):
-        self.kind=name
+    public="this is public"
+    ___private_="this is private"
 
 
 if "__main__" == __name__:
     a=dog()
-    b=dog()
-    print(dog.init)
-    print(a.init)
-    a.init("wang")
-    b.init("miao")
-    print(a.kind)
-    print(b.kind)
-
-    a.kk=123
-    b.kk=234
-    print(a.kk)
-    print(b.kk)
-
+    print(a.public)
+    print(a._dog___private_)
