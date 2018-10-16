9db1897a5c4f1cb0b4872f27f6b8ba37cd6970be
francis <yinghao_liu@163.com>
2018-10-16 04:00:57 +0000

    static and class method

--- feature.py
+++ feature.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python3
 
 class a:
-    me="class a"
+    def show( message):
+        print("this is class a, message is {}, normal".format(message))
 
-class b(a):
-    pass
+class b:
+    @classmethod
+    def show(cls, message):
+        print("this is class b, message is {}, classmethod".format(message))
 
-class c():
-    me="class c"
-
-class d(b, c):
-    pass
-
+class c:
+    @staticmethod
+    def show(message):
+        print("this is class c, message is {}, staticmethod".format(message))
 if "__main__" == __name__:
-    ins=d()
-    print(ins.me) # output "class a"
+    print(a.show)
+    print(b.show)
+    print(c.show)
+    a.show(1)
+    b.show(2)
+    c.show(3)
+    ins1=a()
+    ins2=b()
+    ins3=c()
+    ins2.show(20)
+    ins3.show(30)
+    ins1.show(10)
